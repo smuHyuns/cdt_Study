ffcf20ea47bf8080324d1fe3a8f8460a14439c2e
최현수 <106013493+smuHyuns@users.noreply.github.com>
2024-07-28 02:39:21 +0900

    20240728

--- 금요일코테수업/5주차/2.java
+++ 금요일코테수업/5주차/2.java
@@ -12,14 +12,17 @@ class Solution {
         //시작점, 래버, 종료지점 찾기
         for(int i=0; i<N; i++){
             for(int j=0; j<M; j++){
+                //시작점
                 if(maps[i].charAt(j) == 'S'){
                     startX =  j;
                     startY =  i;
                 }
+                //래버 위치
                 else if(maps[i].charAt(j) == 'L'){
                     leverX =  j;
                     leverY =  i;
                 }
+                //종료지점
                 else if(maps[i].charAt(j) == 'E'){
                     targetX =  j;
                     targetY =  i;
@@ -27,38 +30,41 @@ class Solution {
             }
         }
 
-        //루트까지 찾기 위한 탐색
-        bfs(maps, visited, route, startX, startY, 0);
-        if(!visited[leverY][leverX]) return -1;
-        visited = new boolean[N][M];
-        bfs(maps, visited, route, leverX, leverY, route[leverY][leverX]);
-        if(!visited[targetY][targetX]) return -1;
+        //시작지점 -> 래버까지 찾기 위한 탐색
+        bfs(maps, visited, route, startX, startY, 0); // 이당시 초기값은 0
+        if(!visited[leverY][leverX]) return -1; // 래버를 방문하지 못했다면 = 래버까지 갈 수 없음 = -1을 리턴해준다
+        visited = new boolean[N][M]; // 새로운 탐색을 위한 방문 초기화
+        //래버 -> 종료지점 으로 가기 위한 탐색
+        bfs(maps, visited, route, leverX, leverY, route[leverY][leverX]); // 초기값은 래버까지 도달하는데에 최단거리
+        if(!visited[targetY][targetX]) return -1; // 타겟을 방문하지 못했다면 = 탈출 불가 = -1을 리턴한다.
 
-        return route[targetY][targetX];
+        return route[targetY][targetX]; // 타겟을 방문했다면 거리를 리턴하여준다.
     }
 
     public void bfs(String[] maps, boolean[][] visited, int[][] route, int x, int y, int init){
+        //상화좌우 탐색을 위한 배열 선언
         int[] xrr = {1,-1,0,0};
         int[] yrr = {0,0,1,-1};
 
-        int M = maps.length;
-        int N = maps[0].length();
-        Deque<int[]> q = new ArrayDeque<>();
+        //가로세로길이정의
+        int M = maps.length; //세로길이
+        int N = maps[0].length(); //가로길이
+        Deque<int[]> q = new ArrayDeque<>(); //최단거리 탐색을 위한 bfs를 사용할 것이므로 stack이 아닌 queue를 사용해야함
 
-        q.offer(new int[]{x, y});
-        visited[y][x] = true;
-        route[y][x] = init;
+        q.offer(new int[]{x, y}); //초기값을 넣어주고
+        visited[y][x] = true; // 방문표시
+        route[y][x] = init; // 현재 위치를 전달받은 값으로 초기화시켜준다
 
         while(!q.isEmpty()){
-            int[] top = q.poll();
-            for(int i=0; i<4; i++){
+            int[] top = q.poll(); // 큐 제일 맨 앞의 값 poll
+            for(int i=0; i<4; i++){  // 상하좌우 탐색
                 int dx = top[0] + xrr[i];
                 int dy = top[1] + yrr[i];
-                if( dx >= 0 && dx < N && dy < M && dy >= 0 && !visited[dy][dx]
+                if( dx >= 0 && dx < N && dy < M && dy >= 0 && !visited[dy][dx] // 이동한 거리가 배열범위 안에 있고 방문하지 않았으며, 막혀있지 않다면
                         && maps[dy].charAt(dx) != 'X'){
-                    visited[dy][dx] = true;
-                    route[dy][dx] = route[top[1]][top[0]] + 1;
-                    q.offer(new int[]{dx,dy});
+                    visited[dy][dx] = true; //방문 표시
+                    route[dy][dx] = route[top[1]][top[0]] + 1; //거리값 갱신, 한칸이동이므로 +1
+                    q.offer(new int[]{dx,dy}); //큐에 넣어줌
                 }
             }
         }
--- 금요일코테수업/5주차/3.java
+++ 금요일코테수업/5주차/3.java
@@ -8,12 +8,13 @@ class Solution {
         return answer;
     }
 
+    //전체탐색
     public boolean dfs(String[] place){
         // 맨해튼거리라는건 x1,y1 / x2,y2가 있을때 x1-x2 절대값 + y1-y2 절대값 이 2이하면 안됨
         // 대신 근처에 파티션 있으면 괜찮(X)
         for(int i=0; i<5; i++){
             for(int j=0; j<5; j++){
-                if(place[i].charAt(j) == 'P'){
+                if(place[i].charAt(j) == 'P'){ //사람만났을때 조건 충족하는지 확인함
                     if(!isValid(place,j, i)) return false;
                 }
             }
@@ -22,20 +23,20 @@ class Solution {
     }
 
     public boolean isValid(String[] place, int x, int y){
-        //8가지방향
+        //8가지방향 - 고려해야 할 상하좌우대각선
         int[] xrr = {1,1,1,0,0,-1,-1,-1};
         int[] yrr = {-1,0,1,-1,1,-1,0,1};
 
-        for(int i=0; i<8; i++){
+        for(int i=0; i<8; i++){ // 방향만큼 탐색
             int dx = x;
             int dy = y;
-            for(int j=0; j<2; j++){
+            for(int j=0; j<2; j++){ //x = +2 , y = +2 범위내로 해결하면됨 ( 그 이상의 거리가 되면 뭐가 나오든 괜찮으므로 )
                 dx += xrr[i];
                 dy += yrr[i];
-                if(dx >= 0 && dx < 5 && dy >= 0 && dy < 5){
-                    if(place[dy].charAt(dx)== 'X') break;
-                    else if(place[dy].charAt(dx) == 'P' && Math.abs(dx-x) + Math.abs(dy-y) <= 2){
-                        if(place[y].charAt(dx) != 'X' || place[dy].charAt(x) != 'X') return false;
+                if(dx >= 0 && dx < 5 && dy >= 0 && dy < 5){ //범위를 확인해본다.
+                    if(place[dy].charAt(dx)== 'X') break; //파티션을 만나면 거리가 안될 가능성이 없음
+                    else if(place[dy].charAt(dx) == 'P' && Math.abs(dx-x) + Math.abs(dy-y) <= 2){ // 사람을 만나고, 거리가 충족이 안되었을때
+                        if(place[y].charAt(dx) != 'X' || place[dy].charAt(x) != 'X') return false; // 사이에 파티션으로 막혀있다면 ok
                     }
                 }
             }
